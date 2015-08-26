e0eddd64840b16051ad06f0646efdf7b41566a4a
James Bomotti <zilliahix@gmail.com>
2015-08-25 22:03:49 -0700

    Add top_translate helper for home.erb

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -4,8 +4,10 @@ helpers do
   def top_languages
     Language.joins(:language_learners).group("languages.id").order("count(language_learners.language_id) desc").limit(3)
   end
-end
 
-helpers do
   EasyTranslate.api_key = 'AIzaSyBivCByKj2jybJhv9QWu8q2_oLUXzZIyK8'
+
+  def top_translate(languages)
+    languages[0].name.downcase.to_sym
+  end
 end
