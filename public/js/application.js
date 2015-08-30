93013c521b3b31340a75839bb14cadc33cfeb4a1
James Bomotti <zilliahix@gmail.com>
2015-08-30 16:53:53 -0700

    Add attempts at jQuery randomization of background

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
+  $(function() {
+    var images = ['bg01.jpeg', 'bg02.jpeg', 'bg03.jpeg', 'bg04.jpeg', 'bg05.jpeg', 'bg06.jpeg'];
+    $('#yielded').css({'background-image': 'url(/public/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+   });
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
