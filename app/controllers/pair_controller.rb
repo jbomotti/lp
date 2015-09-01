f3721107147d795283571ad171c272a9c66a5d06
James Bomotti <zilliahix@gmail.com>
2015-08-31 20:04:55 -0700

    Add Message Your Pair button for Twilio notification

--- app/controllers/pair_controller.rb
+++ app/controllers/pair_controller.rb
@@ -1,3 +1,11 @@
+get '/pair/notify' do
+  @user = auth_current_user
+  @pair = User.find(params[:pair])
+  @language = Language.find(params[:lang_id])
+  send_twilio_pairing(@user, @pair, @language)
+  redirect back
+end
+
 get '/pair/:id' do
   @pair = User.find(params[:id])
   @user = auth_current_user
